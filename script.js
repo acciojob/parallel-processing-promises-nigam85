a132dc432047515cfcf16f462ba00be035cc7db1
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-08-24 21:40:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+ function downloadImage(image) {
+            return new Promise((resolve, reject) => {
+                const imgElement = new Image();
+                imgElement.src = image.url;
+
+                imgElement.onload = () => {
+                    resolve(imgElement);
+                };
+
+                imgElement.onerror = () => {
+                    reject(new Error(`Failed to load image's URL: ${image.url}`));
+                };
+            });
+        }
+
+        document.getElementById('download-images-button').addEventListener('click', async () => {
+            const outputDiv = document.getElementById('output');
+            outputDiv.innerHTML = ''; // Clear previous content
+
+            try {
+                const imagePromises = images.map(downloadImage);
+                const downloadedImages = await Promise.all(imagePromises);
+
+                downloadedImages.forEach(imgElement => {
+                    outputDiv.appendChild(imgElement);
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        });
